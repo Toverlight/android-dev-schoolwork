17ccb903647c94a2c8fcede0f13056232df57d17
Toverlight <1312705301@qq.com>
2025-11-16 18:48:21 +0800

    实验三-修复一些问题

--- sy3-listview/README.md
+++ sy3-listview/README.md
@@ -6,46 +6,58 @@
 
 主界面，提供四个按钮，分别跳转到对应的布局页面、弹框
 
-![main.png](img.png)
+![main.png](imgs/img.png)
 
 ## Simple Adapter - ListView
 
+点击时背景红色（点击Elephant项效果）
+
+![sa_bk_red.png](imgs/img_9.png)
+
 图示为点击Lion项的效果
 
-![SimpleAdapter.png](img_1.png)
+![SimpleAdapter.png](imgs/img_1.png)
 
 ## AlertDialog
 
 点击AlertDialog演示 按钮，弹出对话框
 
-![AlertDialog.png](img_2.png)
+![AlertDialog.png](imgs/img_10.png)
+
+点击确定
+
+![ad_confirm.png](imgs/img_2.png)
+
+点击取消
+
+![ad_cancel.png](imgs/img_11.png)
 
 ## XML自定义菜单
 
 ### 一级菜单项
 
-![menu_1.png](img_3.png)
+![menu_1.png](imgs/img_3.png)
 
 ### 二级菜单项
 
 字体大小
 
-![menu_2.png](img_6.png)
+![menu_2.png](imgs/img_6.png)
 
 字体颜色
 
-![menu_3.png](img_5.png)
+![menu_3.png](imgs/img_5.png)
 
 ### 设置效果（图示为字号“大”，字色红色）
 
-![menu_4.png](img_4.png)
+![menu_4.png](imgs/img_4.png)
 
 ## ActionMode上下文菜单
 
 选中效果
 
-![selected.png](img_7.png)
+![selected.png](imgs/img_7.png)
 
 删除后
 
-![deleted.png](img_8.png)
+![deleted.png](imgs/img_8.png)
--- sy3-listview/src/main/java/com/tovl/sy3_listview/MainActivity.java
+++ sy3-listview/src/main/java/com/tovl/sy3_listview/MainActivity.java
@@ -1,12 +1,14 @@
 package com.tovl.sy3_listview;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AlertDialog;
@@ -44,14 +46,39 @@ public class MainActivity extends AppCompatActivity {
 
     private void showCustomAlertDialog() {
         LayoutInflater inflater = getLayoutInflater();
-
         View dialogView = inflater.inflate(R.layout.layout_alert_dialog, null);
 
+        EditText usernameEditText = dialogView.findViewById(R.id.et_username);
+        EditText passwordEditText = dialogView.findViewById(R.id.et_password);
+        Button cancelButton = dialogView.findViewById(R.id.btn_cancel);
+        Button signInButton = dialogView.findViewById(R.id.btn_signin);
+
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-
         builder.setView(dialogView);
-
         AlertDialog alertDialog = builder.create();
+
+        if (alertDialog.getWindow() != null) {
+            alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+        }
+
+        cancelButton.setOnClickListener(v -> {
+            Toast.makeText(this, "点击了取消按钮", Toast.LENGTH_SHORT).show();
+            alertDialog.dismiss(); // 关闭对话框
+        });
+
+        signInButton.setOnClickListener(v -> {
+            String username = usernameEditText.getText().toString();
+            String password = passwordEditText.getText().toString();
+
+            // 简单的登录逻辑示例
+            if ("admin".equals(username) && "123456".equals(password)) {
+                Toast.makeText(this, "登录成功!", Toast.LENGTH_SHORT).show();
+                alertDialog.dismiss(); // 登录成功后关闭对话框
+            } else {
+                Toast.makeText(this, "用户名或密码错误", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         alertDialog.show();
     }
 }
